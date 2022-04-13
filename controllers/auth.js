9ba075ef5c641d4816fa154efb6e882991ccbd62
PunnNopdanai7 <nopdanai1@gmail.com>
2022-04-13 17:21:02 +0700

    Added comments on several files
    List of file that I added comments.
    - controller\auth.js
    - middleware\auth.js
    - model\User.js
    - server.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,84 +1,111 @@
 const User = require("../models/User");
 
+/*
+ * @desc     Resigter user
+ * @route    POST /api/v1/auth/register
+ * @access   Public
+ */
 exports.register = async (req, res, next) => {
-	try {
-		console.log(req.body);
-		const { name, tel, email, password, role } = req.body;
-		const user = await User.create({ name, tel, email, password, role });
-		sendTokenResponse(user, 200, res);
-	} catch (err) {
-		res.status(400).json({ success: false, data: err.message });
-		console.log(err.stack);
-	}
+  try {
+    console.log(`req.body: ${req.body}`);
+    const { name, tel, email, password, role } = req.body;
+
+    /* Create user */
+    const user = await User.create({ name, tel, email, password, role });
+    sendTokenResponse(user, 200, res);
+  } catch (err) {
+    console.log(`err.stack: ${err.stack}`);
+    res.status(400).json({ success: false, data: err.message });
+  }
 };
 
+/*
+ * @desc     Login user
+ * @route    POST /api/v1/auth/login
+ * @access   Public
+ */
 exports.login = async (req, res, next) => {
-	try {
-		const { email, password } = req.body;
-		if (!email || !password) {
-			return res.status(400).json({
-				success: false,
-				msg: "Please provide an email and password",
-			});
-		}
+  try {
+    const { email, password } = req.body;
 
-		const user = await User.findOne({ email }).select("+password");
+    /*Validate email and password */
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        msg: "Please provide an email and password",
+      });
+    }
 
-		if (!user) {
-			return res
-				.status(404)
-				.json({ success: false, msg: "Invalid credentials" });
-		}
+    /*Check for user */
+    const user = await User.findOne({ email }).select("+password");
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, msg: "Invalid credentials" });
+    }
 
-		const isMatch = await user.matchPassword(password);
-		if (!isMatch) {
-			return res.status(401).json({
-				success: false,
-				msg: "Invalid credentials",
-			});
-		}
-		sendTokenResponse(user, 200, res);
-	} catch (err) {
-		return res.status(401).json({
-			success: false,
-			msg: "Cannot convert email or password to string",
-		});
-	}
+    /*Check if password matches */
+    const isMatch = await user.matchPassword(password);
+    if (!isMatch) {
+      return res.status(401).json({
+        success: false,
+        msg: "Invalid credentials",
+      });
+    }
+    sendTokenResponse(user, 200, res);
+  } catch (err) {
+    console.log(`err.stack: ${err.stack}`);
+    return res.status(401).json({
+      success: false,
+      msg: "Cannot convert email or password to string",
+    });
+  }
 };
 
+/* Get token from model1, create cookie and send response */
 const sendTokenResponse = (user, statusCode, res) => {
-	const token = user.getSignedJwtToken();
+  /* Create token */
+  const token = user.getSignedJwtToken();
 
-	const options = {
-		expires: new Date(
-			Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-		),
-		httpOnly: true,
-	};
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
 
-	if (process.env.NODE_ENV === "production") {
-		options.secure = true;
-	}
+  if (process.env.NODE_ENV === "production") {
+    options.secure = true;
+  }
 
-	res.status(statusCode)
-		.cookie("token", token, options)
-		.json({ success: true, token });
+  res
+    .status(statusCode)
+    .cookie("token", token, options)
+    .json({ success: true, token });
 };
 
+/*
+ * @desc     Get current Logged in user
+ * @route    GET /api/v1/auth/me
+ * @access   Private
+ */
 exports.getMe = async (req, res, next) => {
-	const user = await User.findById(req.user.id);
+  const user = await User.findById(req.user.id);
 
-	res.status(200).json({
-		success: true,
-		data: user,
-	});
+  res.status(200).json({ success: true, data: user });
 };
 
+/*
+ * @desc     Log user out, Clear Cookie
+ * @route    GET /api/v1/auth/logout
+ * @access   Private
+ */
 exports.logout = async (req, res, next) => {
-	res.cookie("token", "none", {
-		expires: new Date(Date.now() + 10 * 1000),
-		httpOnly: true,
-	});
+  /* set cookie token to 'none', then response success = 'true'*/
+  res.cookie("token", "none", {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  });
 
-	res.status(200).json({ success: true, data: {} });
+  res.status(200).json({ success: true, data: {} });
 };
--- server.js
+++ server.js
@@ -3,15 +3,21 @@ const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 const connectDB = require("./config/db");
 
+/* Load env vars */
 dotenv.config({ path: "./config/config.env" });
 
+/* Connect to databse */
 connectDB();
 
+/* Route files */
 const auth = require("./routes/auth");
 
 const app = express();
 
+/* Mount routes */
 app.use(express.json());
+
+/* Cookie Parser */
 app.use(cookieParser());
 
 app.use("/api/v1/auth", auth);
@@ -19,13 +25,14 @@ app.use("/api/v1/auth", auth);
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
-	PORT,
-	console.log(
-		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
-	)
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
 
+/* handle unhandled promise rejections */
 process.on("unhandledRejection", (err, promise) => {
-	console.log(`Error: ${err.message}`);
-	server.close(() => process.exit(1));
+  console.log(`Error: ${err.message}`);
+
+  /* Close server & exit process */
+  server.close(() => process.exit(1));
 });
