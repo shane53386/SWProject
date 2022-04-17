00fa0bba0cf74490e1f6149bd581a88827141dc3
shane53386 <mchutipon@gmail.com>
2022-04-17 16:38:40 +0700

    Fix restaurant

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -1,25 +1,26 @@
 const express = require("express");
 const {
-  getRestaurants,
-  getRestaurant,
-  createRestaurant,
-  updateRestaurant,
-  deleteRestaurant,
+	getRestaurants,
+	getRestaurant,
+	createRestaurant,
+	updateRestaurant,
+	deleteRestaurant,
 } = require("../controllers/restaurants");
+const reservationRouter = require("./reservations");
 
 const router = express.Router();
-const app = express();
 const { protect, authorize } = require("../middleware/auth");
 
+router.use("/:restaurantId/reservations/", reservationRouter);
 router
-  .route("/")
-  .get(getRestaurants)
-  .post(protect, authorize("admin"), createRestaurant);
+	.route("/")
+	.get(protect, getRestaurants)
+	.post(protect, authorize("admin"), createRestaurant);
 
 router
-  .route("/:id")
-  .get(getRestaurant)
-  .put(protect, authorize("admin"), updateRestaurant)
-  .delete(protect, authorize("admin"), deleteRestaurant);
+	.route("/:id")
+	.get(protect, getRestaurant)
+	.put(protect, authorize("admin"), updateRestaurant)
+	.delete(protect, authorize("admin"), deleteRestaurant);
 
 module.exports = router;
