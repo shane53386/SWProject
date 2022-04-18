3f557e885b9feb0e15542234c2e1d4427f2de317
PunnNopdanai7 <nopdanai1@gmail.com>
2022-04-18 15:21:47 +0700

    Comment Added on several files

--- models/Reservation.js
+++ models/Reservation.js
@@ -1,14 +1,24 @@
 const mongoose = require("mongoose");
 
 const ReservationSchema = new mongoose.Schema({
-	reservedDate: { type: Object, required: true },
-	user: { type: mongoose.Schema.ObjectId, ref: "User", required: true },
-	restaurant: {
-		type: mongoose.Schema.ObjectId,
-		ref: "Restaurant",
-		required: true,
-	},
-	createdAt: { type: Date, default: Date.now },
+  reservedDate: {
+    type: Object,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  restaurant: {
+    type: mongoose.Schema.ObjectId,
+    ref: "Restaurant",
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 module.exports = mongoose.model("Reservation", ReservationSchema);
